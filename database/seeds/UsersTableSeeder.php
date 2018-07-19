57c1b215492586c311729812d04f16b71e5db0e8
Sergio Torre <kekotorre@gmail.com>
2018-07-19 11:52:12 +0200

    Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "sergio",
+            'apellido_1' => "torre",
+            'apellido_2' => "de la Fuente",
+            'movil' => "618496362",
+            'email' => "kekotorre@gmail.com",
+            'password' => bcrypt('123456'),
+            'admin' => 1,
+        ]);
+    }
+}
