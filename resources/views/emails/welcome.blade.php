99c0014c2ca4a6fa2d3dc04fd4d9152f392e6678
Sergio Torre <kekotorre@gmail.com>
2019-05-23 22:19:48 +0200

    Archivos

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Prueba de envio de correos</h2>
+        <div>
+            <p>Bienvenido al sitio web de {!!$name!!}</p>
+        </div>
+    </body>
+</html>
